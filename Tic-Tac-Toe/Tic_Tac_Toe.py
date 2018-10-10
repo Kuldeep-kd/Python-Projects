ecbf08182085e2e1fde8a9a6a3cc4a3a9261bcfa
Kuldeep Thakre <kuldeep-kd@live.com>
2018-10-11 00:11:43 +0530

    Screen Clear Function Added Comments Improvised the code Added Tie check

--- Tic-Tac-Toe/Tic_Tac_Toe.py
+++ Tic-Tac-Toe/Tic_Tac_Toe.py
@@ -1,6 +1,8 @@
+#Draws Game Board using a list
 def Display(board):
     print(' {} | {} | {} \n-----------\n {} | {} | {} \n-----------\n {} | {} | {} '.format(board[7],board[8],board[9],board[4],board[5],board[6],board[1],board[2],board[3]))
 
+#Let`s player choose a marker
 def Choose_Marker():
     x = input("Please choose a Marker X or O ")
     if x in ['X','O']:
@@ -10,18 +12,22 @@ def Choose_Marker():
         print("Please enter Correct Marker!")
         Choose_Marker()
 
+#Marks the location with the current player`s Marker
 def Place_Marker(board, marker, location):
     board[location] = marker
     return board
 
+#Checks if Player Wins or the Game Ties
 def Has_Won(board, mark):
     
     if(board[1]==board[2]==board[3]!=' ' or board[1]==board[5]==board[9]!=' ' or board[1]==board[4]==board[7]!=' ' or board[2]==board[5]==board[8]!=' ' or board[3]==board[6]==board[9]!=' ' or board[4]==board[5]==board[6]!=' ' or board[7]==board[8]==board[9]!=' ' ):
         print("Congratulations {} is the Winner".format(mark))
         return True
+    elif (Board_Check(board)):
+        print('Ohh! The Game is Tie!')
+        return True
 
-import random
-
+#Randomly selects which player will go first
 def choose_first():
     if random.randint(0,1):
         print("Player {} goes first!".format('X'))
@@ -30,15 +36,15 @@ def choose_first():
         print("Player {} goes first!".format('O'))
         return ['O','X']
 
+#Checks for free space at specified location
 def space_check(board, location):
-    if board[location]==' ':
-        return True
-    else:
-        return False
+    return board[location]==' '
 
+#Checks if Board has any places left
 def Board_Check(board):
     return not ' ' in board
 
+#Checks if the location on Board is available
 def player_choice(board):
     global location
     while True:
@@ -48,19 +54,25 @@ def player_choice(board):
             if 0 < location < 10:
 
                 if space_check(board, location):
+                    os.system('cls')
                     return location
 
         print('Please Choose a Correct Move!')
         continue
         
-
+#Asks user if they want to play again
 def replay():
     return input("You wanna play again? (Yes or No)") == "Yes"
+
+
+#Main Program
+import os
+import random
 
 print('Tic Tac Toe!')
 location = int()
 while True:
-    board = ['#']+[' ']*9
+    board = ['#']+[' ']*9 #This # is to keep out the 0th index
     location = 0
     Choose_Marker()
     marker = choose_first()
@@ -69,15 +81,17 @@ while True:
         player_choice(board)
         board = Place_Marker(board, marker[0], location)
         Display(board)
-        if Has_Won(board, marker[0]) or Board_Check(board):
+        if Has_Won(board, marker[0]):
             break
         # Player 2
         player_choice(board)
         board = Place_Marker(board, marker[1], location)
         Display(board)
         Has_Won(board, marker[1])
-        if Has_Won(board, marker[1]) or Board_Check(board):
+        if Has_Won(board, marker[1]):
             break
 
     if not replay():
+        os.system('cls')
+        print('Thank You for playing!')
         break
